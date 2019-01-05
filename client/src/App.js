2a0b5086accb8922bdb285af8b455601cef9cf91
Jedediah Arnold <jarnold910@gmail.com>
2019-01-05 16:19:15 -0500

    try to fix the links

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import "./App.css";
 import Landing from "./components/Landing";
 import About from "./components/About";
@@ -12,7 +12,7 @@ class App extends Component {
           <a class="navbar-brand" href="/">
             Recoger
           </a>
-          <a href="/about">About</a>
+          <Link to="/about/">About</Link>
           <button
             class="navbar-toggler"
             type="button"
