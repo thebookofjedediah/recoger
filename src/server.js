f9016b089c3a4c712a0d956088cc92de94f9b90e
Jedediah Arnold <jarnold910@gmail.com>
2019-01-05 15:17:26 -0500

    attempting to do it the wayother way

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const app = require("./app");
 const http = require("http");
+const express = require("express");
+const path = require("path");
 
 const port = normalizePort(process.env.PORT || "5000");
 app.set("port", port);
@@ -17,6 +19,13 @@ function normalizePort(val) {
     return port;
   }
   return false;
+}
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 server.on("listening", () => {
