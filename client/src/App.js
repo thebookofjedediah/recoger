fb91718944b2d3851bf92f74ddc0962781e51e63
Jedediah Arnold <jarnold910@gmail.com>
2019-01-05 14:28:26 -0500

    nothing for prod yet

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 import "./App.css";
 import Landing from "./components/Landing";
 import About from "./components/About";
