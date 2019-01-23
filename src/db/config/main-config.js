89f31e31603447916d1db6bebf795301cfcd90fd
Jedediah Arnold <jarnold910@gmail.com>
2019-01-23 14:16:57 -0500

    added a main config...

---
+++ src/db/config/main-config.js
@@ -0,0 +1,27 @@
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+const session = require("express-session");
+const flash = require("express-flash");
+const passportConfig = require("./passport-config");
+require("dotenv").config();
+
+module.exports = {
+  init(app, express) {
+    app.use(bodyParser.json());
+    app.use(expressValidator());
+    app.use(
+      session({
+        secret: process.env.cookieSecret,
+        resave: false,
+        saveUninitialized: false,
+        cookie: { maxAge: 1.21e9 }
+      })
+    );
+    app.use(flash());
+    passportConfig.init(app);
+    app.use((req, res, next) => {
+      res.locals.currentUser = req.user;
+      next();
+    });
+  }
+};
